4525178619a877761ef465d53e4352e9c0219d59
Mikhail Matz <matz@utexas.edu>
2017-12-29 09:29:32 -0600

    new switch oligos

---
+++ tagseq_clipper.pl
@@ -0,0 +1,96 @@
+#!/usr/bin/perl
+
+$usage= "
+
+tagseq_clipper.pl  : 
+
+Clips 5'-leader off Illumina fastq reads in RNA-seq
+
+Removes duplicated reads sharing the same degenerate header and 
+the first 20 bases of the sequence (reads containing N bases in this
+region are discarded, too)
+
+prints to STDOUT
+
+arguments:
+1 : fastq file name
+2 : string to define the leading sequence, default '[ATGC]?[ATGC][AC][AT]GGG+|[ATGC]?[ATGC]TGC[AC][AT]GGG+|[ATGC]?[ATGC]GC[AT]TC[ACT][AC][AT]GGG+'
+'keep' : optional flag to say whether the sequences without leader should be kept. 
+		 By default, they are discarded.
+
+Example:
+tagseq_clipper.pl D6.fastq
+
+					 
+";
+
+my $fq=shift or die $usage;
+my $lead="";
+my $keep=0;
+if ($ARGV[0]) { $lead=$ARGV[0];}
+else { $lead="[ATGC]?[ATGC][AC][AT]GGG+|[ATGC]?[ATGC]TGC[AC][AT]GGG+|[ATGC]?[ATGC]GC[AT]TC[ACT][AC][AT]GGG+";}
+if ($ARGV[1]) {$keep=1;}
+my $trim=0;
+my $name="";
+my $name2="";
+my $seq="";
+my $qua="";
+my %seen={};
+open INP, $fq or die "cannot open file $fq\n";
+my $ll=3;
+my $nohead=0;
+my $dups=0;
+my $ntag;
+my $tot=0;
+my $goods=0;
+while (<INP>) {
+	if ($ll==3 && $_=~/^(\@.+)$/ ) {
+		$tot++;
+		$name2=$1; 
+		if ($seq=~/^($lead)(.+)/) {	
+			my $start=substr($2,0,20);
+			my $idtag=$1.$start;
+			if (!$seen{$idtag} and $idtag!~/N/) {
+				$seen{$idtag}=1;
+				$trim=length($1);
+				print "$name\n$2\n+\n",substr($qua,$trim),"\n";
+				$goods++;
+			}
+			elsif ($seen{$idtag}) { $dups++; }
+			else { $ntag++; }
+		}
+		elsif ($keep and $name) { print "$name\n$seq\n+\n",$qua,"\n";}
+		else {$nohead++;}
+		$seq="";
+		$ll=0;
+		$qua="";
+		@sites=();
+		$name=$name2;
+	}
+	elsif ($ll==0){
+		chomp;
+		$seq=$_;
+		$ll=1;
+	}
+	elsif ($ll==2) { 
+		chomp;
+		$qua=$_; 
+		$ll=3; 
+	}
+	else { $ll=2;}
+}
+
+                if ($seq=~/^($lead)(.+)/) {
+                        my $start=substr($2,0,20);
+                        my $idtag=$1.$start;
+                        if (!$seen{$idtag} and $idtag!~/N/) {
+                                $seen{$idtag}=1;
+                                $trim=length($1);
+                                print "$name\n$2\n+\n",substr($qua,$trim),"\n";
+                                $goods++;
+                        }
+                        elsif ($seen{$idtag}) { $dups++; }
+                        else { $ntag++; }
+                }
+elsif ($keep and $name) { print "$name\n$seq\n+\n",$qua,"\n";}
+warn "$fq\ttotal:$tot\tgoods:$goods\tdups:$dups\tnoheader:$nohead\tN.in.header:$ntag\n";
---
+++ tagseq_trim_launch_bgi.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+my $usage= "
+
+Prints out list of commands for launcher_creator.py 
+to trim Illumina RNA-seq reads
+
+Arguments:
+1: glob to fastq files
+2: optional, the position of name-deriving string in the file name
+	if separated by underscores, 
+	such as: input file Sample_RNA_2DVH_L002_R1.cat.fastq
+	specifying arg2 as \'3\' would create output file with a name \'2DVH.fastq'	
+			
+";
+
+if (!$ARGV[0]) { die $usage;}
+my $glob=$ARGV[0];
+
+opendir THIS, ".";
+my @fqs=grep /$glob/,readdir THIS;
+my $outname="";
+
+foreach $fqf (@fqs) {
+	if ($ARGV[1]) {
+		my @parts=split('_',$fqf);
+		$outname=$parts[$ARGV[1]-1].".trim";
+	}
+	else { $outname=$fqf.".trim";}
+	print "tagseq_clipper.pl $fqf | fastx_clipper -a AAAAAAAA -l 20 | fastx_clipper -a AGATCGGAAG -l 20 | fastq_quality_filter -q 20 -p 90 >$outname\n";
+}
