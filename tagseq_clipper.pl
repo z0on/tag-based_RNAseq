e1fdc87795931b172013ade81dcd84f32b0a03e8
Mikhail Matz <matz@utexas.edu>
2019-10-10 12:46:04 -0500

    clipper mod

--- tagseq_clipper.pl
+++ tagseq_clipper.pl
@@ -15,8 +15,6 @@ prints to STDOUT
 arguments:
 1 : fastq file name
 2 : string to define the leading sequence, default '[ATGC]?[ATGC][AC][AT]GGG+|[ATGC]?[ATGC]TGC[AC][AT]GGG+|[ATGC]?[ATGC]GC[AT]TC[ACT][AC][AT]GGG+'
-'keep' : optional flag to say whether the sequences without leader should be kept. 
-		 By default, they are discarded.
 
 Example:
 tagseq_clipper.pl D6.fastq
@@ -26,10 +24,8 @@ tagseq_clipper.pl D6.fastq
 
 my $fq=shift or die $usage;
 my $lead="";
-my $keep=0;
 if ($ARGV[0]) { $lead=$ARGV[0];}
 else { $lead="[ATGC]?[ATGC][AC][AT][AT][AC][AT][ACT]GGG+|[ATGC]?[ATGC][AC][AT]GGG+|[ATGC]?[ATGC]TGC[AC][AT]GGG+|[ATGC]?[ATGC]GC[AT]TC[ACT][AC][AT]GGG+";}
-if ($ARGV[1]) {$keep=1;}
 my $trim=0;
 my $name="";
 my $name2="";
@@ -40,7 +36,7 @@ open INP, $fq or die "cannot open file $fq\n";
 my $ll=3;
 my $nohead=0;
 my $dups=0;
-my $ntag;
+my $ntag=0;
 my $tot=0;
 my $goods=0;
 while (<INP>) {
@@ -58,9 +54,10 @@ while (<INP>) {
 			}
 			elsif ($seen{$idtag}) { $dups++; }
 			else { $ntag++; }
+		} 
+		elsif ($tot>1) {
+			$nohead++;
 		}
-		elsif ($keep and $name) { print "$name\n$seq\n+\n",$qua,"\n";}
-		else {$nohead++;}
 		$seq="";
 		$ll=0;
 		$qua="";
@@ -80,17 +77,20 @@ while (<INP>) {
 	else { $ll=2;}
 }
 
-                if ($seq=~/^($lead)(.+)/) {
-                        my $start=substr($2,0,20);
-                        my $idtag=$1.$start;
-                        if (!$seen{$idtag} and $idtag!~/N/) {
-                                $seen{$idtag}=1;
-                                $trim=length($1);
-                                print "$name\n$2\n+\n",substr($qua,$trim),"\n";
-                                $goods++;
-                        }
-                        elsif ($seen{$idtag}) { $dups++; }
-                        else { $ntag++; }
-                }
-elsif ($keep and $name) { print "$name\n$seq\n+\n",$qua,"\n";}
+if ($seq=~/^($lead)(.+)/) {
+		my $start=substr($2,0,20);
+		my $idtag=$1.$start;
+		if (!$seen{$idtag} and $idtag!~/N/) {
+				$seen{$idtag}=1;
+				$trim=length($1);
+				print "$name\n$2\n+\n",substr($qua,$trim),"\n";
+				$goods++;
+		}
+		elsif ($seen{$idtag}) { $dups++; }
+		else { $ntag++; }
+}
+elsif ($tot>1) {
+	$nohead++;
+}
+
 warn "$fq\ttotal:$tot\tgoods:$goods\tdups:$dups\tnoheader:$nohead\tN.in.header:$ntag\n";
